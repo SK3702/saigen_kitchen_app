5ad356399302e4c0e3477c7215a424ff851ff973
Seigo Kato <seigokato618@gmail.com>
2025-01-11 03:24:10 +0900

    CircleCiのエラーを修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads"])
+  end
+
   config.include FactoryBot::Syntax::Methods
   config.before(:each, type: :system) do
     driven_by :rack_test
