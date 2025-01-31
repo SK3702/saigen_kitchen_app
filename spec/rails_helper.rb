3b77b0ea8c9688c854e64228726c057032811e5a
Seigo Kato <seigokato618@gmail.com>
2025-01-31 11:55:52 +0900

    add rspecでjsの動作をテストするために追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,30 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-# Prevent database truncation if the environment is production
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
+
+Capybara.javascript_driver = :selenium_chromium_remote
+
+Capybara.register_driver :selenium_chromium_remote do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  #options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+
+  Capybara::Selenium::Driver.new(app,
+    browser: :remote,
+    url: "http://chrome:4444/wd/hub",
+    options: options
+  )
+end
+
+Capybara.server_host = ENV['CAPYBARA_SERVER_HOST'] || "0.0.0.0"
+Capybara.server_port = ENV['CAPYBARA_SERVER_PORT'] || 3001
+Capybara.app_host = ENV['CAPYBARA_APP_HOST'] || "http://web:3001"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -70,16 +90,16 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
 
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chromium_remote
+  end
+
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
 
   config.include FileHelper, type: :request
   config.include FileHelper, type: :system
   config.include FileHelper, type: :model
-
-  config.before(:each, js: true) do
-    driven_by :selenium_chrome_headless
-  end
 end
 
 Shoulda::Matchers.configure do |config|
