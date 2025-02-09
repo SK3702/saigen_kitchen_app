25a16b25d03f4a3fb60c4e5d51843a36bf3a9bfa
Seigo Kato <seigokato618@gmail.com>
2025-02-09 22:11:23 +0900

    fix rails_admin導入によるcircleciのエラー解消

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(item => item.loader === 'sass-loader')
+
+sassLoaderConfig.options = Object.assign({}, sassLoaderConfig.options, {
+  implementation: require('sass'),
+  sassOptions: {
+    outputStyle: 'expanded'
+  }
+})
+
 module.exports = environment
